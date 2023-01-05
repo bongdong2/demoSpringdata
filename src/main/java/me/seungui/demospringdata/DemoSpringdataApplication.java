5a13ec7ed34c0cc0f94a591db344b266cb7ef079
bongdong2 <seungui821@ohmycompany.com>
2023-01-05 11:08:01 +0900

    save

--- src/main/java/me/seungui/demospringdata/DemoSpringdataApplication.java
+++ src/main/java/me/seungui/demospringdata/DemoSpringdataApplication.java
@@ -2,8 +2,10 @@ package me.seungui.demospringdata;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@EnableJpaRepositories
 public class DemoSpringdataApplication {
 
   public static void main(String[] args) {
--- src/main/java/me/seungui/demospringdata/JpaRunner.java
+++ src/main/java/me/seungui/demospringdata/JpaRunner.java
@@ -1,13 +1,7 @@
 package me.seungui.demospringdata;
 
-import java.util.List;
-import javax.persistence.EntityManager;
-import javax.persistence.PersistenceContext;
-import javax.persistence.TypedQuery;
-import javax.persistence.criteria.CriteriaBuilder;
-import javax.persistence.criteria.CriteriaQuery;
-import javax.persistence.criteria.Root;
 import javax.transaction.Transactional;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.stereotype.Component;
@@ -16,31 +10,12 @@ import org.springframework.stereotype.Component;
 @Transactional
 public class JpaRunner implements ApplicationRunner {
 
-  @PersistenceContext
-  EntityManager entityManager;
+  @Autowired
+  PostRepository repository;
 
   @Override
   public void run(ApplicationArguments args) {
-    // Dirty checking : 객체의 상태변화 감지
-    // Write Behind : 객체의 상태변화를 데이터베이스에 최대한 늦게 반영
-
-    // JQPL : 테이블이 아닌 엔티티 객체 모델 기반으로 쿼리 작성
-//    TypedQuery<Post> query = entityManager.createQuery("SELECT p FROM Post AS p", Post.class);
-//    List<Post> posts = query.getResultList();
-//    posts.forEach(System.out::println);
-
-    // Criteria
-//    CriteriaBuilder builder = entityManager.getCriteriaBuilder();
-//    CriteriaQuery<Post> query = builder.createQuery(Post.class);
-//    Root<Post> root = query.from(Post.class);
-//    query.select(root);
-//
-//    List<Post> posts = entityManager.createQuery(query).getResultList();
-//    posts.forEach(System.out::println);
-
-    // Native
-    List<Post> posts = entityManager.createNativeQuery("select * from post", Post.class).getResultList();
-    posts.forEach(System.out::println);
+    repository.findAll().forEach(System.out::println);
   }
 }
 
